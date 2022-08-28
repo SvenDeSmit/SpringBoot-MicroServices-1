3ec7dc1499bb77ed97dd7a9cdc6344a5277f203b
SvenDeSmit <sven.desmit@telenet.be>
2022-08-28 18:59:06 +0200

    First commit

---
+++ src/main/java/be/sven/microservices/SpringBootMicroServices1Application.java
@@ -0,0 +1,13 @@
+package be.sven.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroServices1Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/sven/microservices/SpringBootMicroServices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.sven.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
